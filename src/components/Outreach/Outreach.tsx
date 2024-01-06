9df1b372aae67bcffe525d874f3a71ae58782d2a
sachin singh <77788607+sachinsingh24@users.noreply.github.com>
2024-01-06 12:38:24 +0530

    addedcontent

--- src/components/Outreach/Outreach.tsx
+++ src/components/Outreach/Outreach.tsx
@@ -1,10 +1,9 @@
-import "./Outreach.css";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
+/** @format */
+
+import './Outreach.css';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 const Outreach = () => {
-
-
-
   const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
@@ -23,23 +22,24 @@ const Outreach = () => {
     },
   };
   const images = [
-    "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-    "https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
-    "https://images.unsplash.com/photo-1550133730-695473e544be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-    "https://images.unsplash.com/photo-1550167164-1b67c2be3973?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-    "https://images.unsplash.com/photo-1550338861-b7cfeaf8ffd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-    "https://images.unsplash.com/photo-1550223640-23097fc71cb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-    "https://images.unsplash.com/photo-1550353175-a3611868086b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-    "https://images.unsplash.com/photo-1550330039-a54e15ed9d33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-    "https://images.unsplash.com/photo-1549737328-8b9f3252b927?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-    "https://images.unsplash.com/photo-1549833284-6a7df91c1f65?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-    "https://images.unsplash.com/photo-1549985908-597a09ef0a7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-    "https://images.unsplash.com/photo-1550064824-8f993041ffd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    'https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1550133730-695473e544be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1550167164-1b67c2be3973?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1550338861-b7cfeaf8ffd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1550223640-23097fc71cb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1550353175-a3611868086b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1550330039-a54e15ed9d33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1549737328-8b9f3252b927?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1549833284-6a7df91c1f65?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1549985908-597a09ef0a7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1550064824-8f993041ffd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
   ];
 
   return (
-    <section className="outreach-section">
-      <div className="social-media container">
+    <section className='outreach-section'>
+      <div className='anchor' id='Social'></div>
+      <div className='social-media container'>
         <h2>Social media clips etc</h2>
 
         <Carousel
@@ -50,40 +50,34 @@ const Outreach = () => {
           autoPlay={true}
           autoPlaySpeed={2500}
           rewind={true}
-          itemClass="image-item"
-          responsive={responsive}
-        >
-          {images.slice(0, 5).map((image, index) => {
+          itemClass='image-item'
+          responsive={responsive}>
+          {images.slice(0, 5).map((item, index) => {
             return (
               <img
                 key={index}
-                src={image}
-                alt={`Image ${index + 1}`}
-                style={{ width: "100%", height: "100%" }}
+                src={item}
+                alt={`social images${index + 1}`}
+                style={{ width: '100%', height: '100%' }}
               />
             );
           })}
-      </Carousel>
-
-
-
-
-
-
-
-
+        </Carousel>
       </div>
-      <div className="register container">
+      <div className='anchor' id='Register'></div>
+      <div className='register container pb-5'>
         <h2>Register</h2>
         <p>
           The R&D fair is open to all Academia & Industry and no entry fee or
           participation registration is mandatory. However, you may register at
           the following link for convenient experience and regular updates of
-          the event:{" "}
-          <a href="https://forms.gle/4q2syxLsh5C9AmSt5">
-            https://forms.gle/4q2syxLsh5C9AmSt5
-          </a>
+          the event:{' '}
         </p>
+        <a href='https://forms.gle/4q2syxLsh5C9AmSt5'>
+          <button type='button' className='btn btn-secondary btn-md'>
+            Register Now
+          </button>
+        </a>
       </div>
     </section>
   );
