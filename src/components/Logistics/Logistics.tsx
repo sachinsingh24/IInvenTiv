9a0882bbe4657a99861c2002f46e2f71ee78fd24
sachin singh <77788607+sachinsingh24@users.noreply.github.com>
2024-01-06 19:58:17 +0530

    updaete

--- src/components/Logistics/Logistics.tsx
+++ src/components/Logistics/Logistics.tsx
@@ -9,7 +9,7 @@ const Logistics = () => {
       <div className='how-to-reach container'>
         <h2>How to Reach IIT Hyderabad</h2>
         <div className='reach'>
-          <div>
+          <div className='map-loaction'>
             <h4>IITH HYDERABAD MAP LOCATION</h4>
             <iframe
               title='IITH HYDERABAD MAP'
@@ -18,93 +18,126 @@ const Logistics = () => {
               height='400px'
               style={{ borderRadius: '15px' }}></iframe>
           </div>
-          <h4>From Airport (64km)</h4>
-          <ul>
-            <li>Get on ORR in Gandiguda from Airport Approach Road</li>
-            <li>Follow ORR to Muthangi, Hyderabad. Take exit 3 from ORR</li>
-            <li>
-              Drive to Mumbai Hwy/NH 9 for 15 km, you will find the sign board
-              of IITH on Right side
-            </li>
-          </ul>
-          <h4>From Mahatma Gandhi Bus Station (54km)</h4>
-          <ul>
-            <li>Head north towards Mumbai Hwy/NH 65</li>
-            <li>Get Via Kukatpally, Patancheruvu</li>
-            <li>Follow ORR to Mumbai Hwy/NH 65</li>
-            <li>Merge onto ORR</li>
-            <li>Take exit towards Mumbai/NH65Take exit towards Mumbai/NH65</li>
-            <li>
-              Drive to Mumbai Hwy/NH 65 for 15 km, you will find the sign board
-              of IITH on Right side
-            </li>
-          </ul>
-          <h4>From Secunderabad Railway Station (50km)</h4>
-          <ul>
-            <li>Take SD Road to Mahatma Gandhi Rd/NH 7 in Hyderabad</li>
-            <li>Follow NH 7 to Old Airport Rd in New Bowenpally</li>
-            <li>Continue on Old Airport Rd to Bhavani Nagar</li>
-            <li>Drive to Mumbai Hwy/NH 65</li>
-            <li>
-              Continue to drive on Mumbai Hwy/NH 65, you will find the sign
-              board of IITH on Right side
-            </li>
-          </ul>
-          <h4>From Hitech City (38km)</h4>
-          <ul>
-            <li>
-              Take Gachibowli – Miyapur Rd to NH65 in Hafeezpet 19 min (6.9 km)
-            </li>
-            <li>Drive to NH65 in Khandi</li>
-          </ul>
-          <h4>Metro Route</h4>
-          <ul>
-            <li>
-              Reach the Secunderabad-East Metro station which is about 600 mtrs
-              from the Secunderabad Railway station.
-            </li>
-            <li>
-              Take a ticket to Miyapur Station. You need to change metros to
-              reach Miyapur Station.
-            </li>
-            <li>Board a metro and get down in Ameerpet Station.</li>
-            <li>Board a metro towards Miyapur Station.</li>
-            <li>Take a bus from Miyapur Bus-stop to Patancheru Bus Station.</li>
-            <li>
-              Take a bus to Sangareddy Town and you will find the sign board of
-              IITH on Right side.
-            </li>
-          </ul>
-          <h4>MMTS Route</h4>
-          <ul>
-            <li>
-              Take an MMTS from Secunderabad Railway Station to Lingampally
-              Railway Station.
-            </li>
-            <li>
-              Take a bus from Lingampally Railway Station to Lingampally Bus
-              stand.
-            </li>
-            <li>Take a bus to Patancheru Bus stand.</li>
-            <li>
-              Take a bus to Sangareddy Town and you will find the sign board of
-              IITH on Right side.
-            </li>
-          </ul>
-          <h4>Important Contacts</h4>
+          <div className='other-ways'>
+            <div className='row justify-content-center'>
+              <div className='col-md-6 my-2 px-2'>
+                <div className='card-layout'>
+                  <h5>From Airport (64km)</h5>
+                  <ul>
+                    <li>Get on ORR in Gandiguda from Airport Approach Road</li>
+                    <li>
+                      Follow ORR to Muthangi, Hyderabad. Take exit 3 from ORR
+                    </li>
+                    <li>
+                      Drive to Mumbai Hwy/NH 9 for 15 km, you will find the sign
+                      board of IITH on Right side
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className='col-md-6 my-2 px-2'>
+                <div className='card-layout'>
+                  <h5>From Mahatma Gandhi Bus Station (54km)</h5>
+                  <ul>
+                    <li>Head north towards Mumbai Hwy/NH 65</li>
+                    <li>Get Via Kukatpally, Patancheruvu</li>
+                    <li>Follow ORR to Mumbai Hwy/NH 65</li>
+                    <li>Merge onto ORR</li>
+                    <li>
+                      Take exit towards Mumbai/NH65Take exit towards Mumbai/NH65
+                    </li>
+                    <li>
+                      Drive to Mumbai Hwy/NH 65 for 15 km, you will find the
+                      sign board of IITH on Right side
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className='col-md-6 my-2 px-2'>
+                <div className='card-layout'>
+                  <h5>From Secunderabad Railway Station (50km)</h5>
+                  <ul>
+                    <li>Take SD Road to Mahatma Gandhi Rd/NH 7 in Hyderabad</li>
+                    <li>Follow NH 7 to Old Airport Rd in New Bowenpally</li>
+                    <li>Continue on Old Airport Rd to Bhavani Nagar</li>
+                    <li>Drive to Mumbai Hwy/NH 65</li>
+                    <li>
+                      Continue to drive on Mumbai Hwy/NH 65, you will find the
+                      sign board of IITH on Right side
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className='col-md-6 my-2 px-2'>
+                <div className='card-layout'>
+                  <h5>From Hitech City (38km)</h5>
+                  <ul>
+                    <li>
+                      Take Gachibowli – Miyapur Rd to NH65 in Hafeezpet 19 min
+                      (6.9 km)
+                    </li>
+                    <li>Drive to NH65 in Khandi</li>
+                  </ul>
+                </div>
+              </div>
+              <div className='col-md-6 my-2 px-2'>
+                <div className='card-layout'>
+                  <h5>Metro Route</h5>
+                  <ul>
+                    <li>
+                      Reach the Secunderabad-East Metro station which is about
+                      600 mtrs from the Secunderabad Railway station.
+                    </li>
+                    <li>
+                      Take a ticket to Miyapur Station. You need to change
+                      metros to reach Miyapur Station.
+                    </li>
+                    <li>Board a metro and get down in Ameerpet Station.</li>
+                    <li>Board a metro towards Miyapur Station.</li>
+                    <li>
+                      Take a bus from Miyapur Bus-stop to Patancheru Bus
+                      Station.
+                    </li>
+                    <li>
+                      Take a bus to Sangareddy Town and you will find the sign
+                      board of IITH on Right side.
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className='col-md-6 my-2 px-2'>
+                <div className='card-layout'>
+                  <h5>MMTS Route</h5>
+                  <ul>
+                    <li>
+                      Take an MMTS from Secunderabad Railway Station to
+                      Lingampally Railway Station.
+                    </li>
+                    <li>
+                      Take a bus from Lingampally Railway Station to Lingampally
+                      Bus stand.
+                    </li>
+                    <li>Take a bus to Patancheru Bus stand.</li>
+                    <li>
+                      Take a bus to Sangareddy Town and you will find the sign
+                      board of IITH on Right side.
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <h3>Important Contacts</h3>
           <ul>
             <li>Guest House Coordinator: </li>
             <li>Student Hostel Coordinator:</li>
             <li>Security:</li>
           </ul>
           <h4>Transportation</h4>
-          <ul>
-            <li>
-              Transportation will be provided for all the participants on
-              January 19, 2024 from Airport/Railway Stations/Miyapur Metro
-              Station
-            </li>
-          </ul>
+          <span>
+            Transportation will be provided for all the participants on January
+            19, 2024 from Airport/Railway Stations/Miyapur Metro Station
+          </span>
         </div>
       </div>
       <div className='anchor' id='hotels'></div>
